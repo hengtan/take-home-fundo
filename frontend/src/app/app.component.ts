b8a38ce673c153bdd330939bfa7018b6368638d0
hengtan <hengjt1@gmail.com>
2025-07-30 12:49:58 -0300

    feat: update app component to include RouterLink and RouterLinkActive for improved navigation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { LoanListComponent } from './components/loan-list/loan-list.component'; // ajuste caminho se necessário
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet
-    // ,LoanListComponent    // Se estiver usando diretamente (mas no seu caso agora é via rota)
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './app.component.html'
 })
