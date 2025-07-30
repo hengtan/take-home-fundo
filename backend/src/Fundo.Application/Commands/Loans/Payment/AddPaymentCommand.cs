7124fa3a2ba79d493e777f374ff49bc5f23f3278
hengtan <hengjt1@gmail.com>
2025-07-30 16:05:33 -0300

    chore: change AddPaymentCommand to an abstract class

--- backend/src/Fundo.Application/Commands/Loans/Payment/AddPaymentCommand.cs
+++ backend/src/Fundo.Application/Commands/Loans/Payment/AddPaymentCommand.cs
@@ -4,7 +4,7 @@ using Unit = Fundo.Application.Common.Results.Unit;
 
 namespace Fundo.Application.Commands.Loans.Payment;
 
-public class AddPaymentCommand : IRequest<Unit>, IRequest<Result<Unit>>
+public abstract class AddPaymentCommand : IRequest<Unit>, IRequest<Result<Unit>>
 {
     public Guid LoanId { get; init; }
     public decimal Amount { get; init; }
