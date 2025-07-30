53afb95d2663a99687d20566eccd546ea4a67af9
hengtan <hengjt1@gmail.com>
2025-07-30 12:56:36 -0300

    feat: add LoanDetailComponent for viewing loan details by ID and update routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoanListComponent } from './components/loan-list/loan-list.component';
 import {HomeComponent} from './components/home/ home.component';
+import {LoanDetailComponent} from './components/loan-detail/loan-detail.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },           // página inicial
   { path: 'loans', component: LoanListComponent },  // rota de lista de empréstimos
-  // futuras rotas:
-  // { path: 'another-endpoint', component: AnotherComponent },
+  { path: 'loan-detail', component: LoanDetailComponent }, // acesso via input
 ];
